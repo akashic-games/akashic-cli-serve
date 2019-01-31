36490f7b907aed1196abce657248505fc28f3f32
dera- <alchemy.and.fairy@gmail.com>
2019-01-31 17:14:32 +0900

    reflect pr-comment

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
+import * as util from "util";
 import * as http from "http";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as socketio from "socket.io";
 import * as commander from "commander";
-import * as util from "util";
 import chalk from "chalk";
 import { PlayManager, RunnerManager, setSystemLogger } from "@akashic/headless-driver";
 import { Global } from "./common/Global";
@@ -22,7 +22,7 @@ declare const global: Global;
 
 // 渡されたパラメータを全てstringに変換する
 // chalkを使用する場合、ログ出力時objectの中身を展開してくれないためstringに変換する必要がある
-function convertStrings(params: any[]): string[] {
+function convertToStrings(params: any[]): string[] {
 	return params.map(param => {
 		if (typeof param === "object") {
 			return util.inspect(param, {depth: null});
@@ -44,7 +44,7 @@ export function run(argv: any): void {
 		.usage("[options] <gamepath>")
 		.option("-p, --port <port>", `The port number to listen. default: ${DEFAULT_PORT}`, (x => parseInt(x, 10)), DEFAULT_PORT)
 		.option("-H, --hostname <hostname>", `The host name of the server. default: ${DEFAULT_HOST}`, DEFAULT_HOST)
-		.option("-v, --verbose", `display detail information on console.`)
+		.option("-v, --verbose", `Display detailed information on console.`)
 		.parse(argv);
 
 	if (isNaN(commander.port)) {
@@ -57,31 +57,27 @@ export function run(argv: any): void {
 	if (commander.verbose) {
 		setSystemLogger({
 			info: (...messages: any[]) => {
-				console.log(chalk.gray(...convertStrings(messages)));
+				console.log(chalk.gray(...convertToStrings(messages)));
 			},
 			debug: (...messages: any[]) => {
-				console.log(chalk.gray(...convertStrings(messages)));
+				console.log(chalk.gray(...convertToStrings(messages)));
 			},
 			warn: (...messages: any[]) => {
-				console.warn(chalk.yellow(...convertStrings(messages)));
+				console.warn(chalk.yellow(...convertToStrings(messages)));
 			},
 			error: (...messages: any[]) => {
-				console.error(chalk.red(...convertStrings(messages)));
+				console.error(chalk.red(...convertToStrings(messages)));
 			}
 		});
 	} else {
 		setSystemLogger({
-			info: (...messages: any[]) => {
-				// {}だけだとtslintでエラーになるのでコメントアウトだけ入れる
-			},
-			debug: (...messages: any[]) => {
-				// {}だけだとtslintでエラーになるのでコメントアウトだけ入れる
-			},
+			info: (...messages: any[]) => {},
+			debug: (...messages: any[]) => {},
 			warn: (...messages: any[]) => {
-				// {}だけだとtslintでエラーになるのでコメントアウトだけ入れる
+				console.warn(chalk.yellow(...convertToStrings(messages)));
 			},
 			error: (...messages: any[]) => {
-				console.error(chalk.red(...convertStrings(messages)));
+				console.error(chalk.red(...convertToStrings(messages)));
 			}
 		});
 	}
